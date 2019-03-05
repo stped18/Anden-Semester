2b9971e92e2a7192bee4cc2044487813cf569d06
Steffen Vitten Pedersen <stped18@student.sdu.dk>
2019-03-05 12:51:02 +0100

    rettelse af fejl

--- ProjektMappen/MMMI/src/mmmi/MMMI.java
+++ ProjektMappen/MMMI/src/mmmi/MMMI.java
@@ -6,11 +6,9 @@
 package mmmi;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,34 +16,22 @@ import javafx.stage.Stage;
  * @author steff
  */
 public class MMMI extends Application {
-          
-          @Override
-          public void start(Stage primaryStage) {
-                    Button btn = new Button();
-                    btn.setText("Say 'Hello World'");
-                    btn.setOnAction(new EventHandler<ActionEvent>() {
-                              
-                              @Override
-                              public void handle(ActionEvent event) {
-                                        System.out.println("Hello World!");
-                              }
-                    });
-                    
-                    StackPane root = new StackPane();
-                    root.getChildren().add(btn);
-                    
-                    Scene scene = new Scene(root, 300, 250);
-                    
-                    primaryStage.setTitle("Hello World!");
-                    primaryStage.setScene(scene);
-                    primaryStage.show();
-          }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
-          /**
-           * @param args the command line arguments
-           */
-          public static void main(String[] args) {
-                    launch(args);
-          }
-          
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }

01e5be5d2bd4307f18b0a3a74257eb98298184f9
Steffen Vitten <Steffen@vittech.dk>
2019-05-05 18:33:07 +0200

    her

---
+++ ProjektMappen/MMMI/src/mmmi/UI/Main/CreateCase/caseOpening/CaseOpenMainTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mmmi.UI.Main.createCase.caseOpening;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author steff
+ */
+public class CaseOpenMainTest extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("caseOpeningFXML.fxml"));
+
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("MMMI");
+        stage.show();
+    }
+
+}
+

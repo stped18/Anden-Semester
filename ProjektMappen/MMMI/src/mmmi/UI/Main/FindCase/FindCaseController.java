b267ede20a1c06d614fbd64b10b77dc1ef3eddda
Unknown <mjako18@student.sdu.dk>
2019-05-15 20:58:46 +0200

    Minor changes
    Changed the fxml and the controller.
    Added beginning for searching.

--- ProjektMappen/MMMI/src/mmmi/UI/Main/FindCase/FindCaseController.java
+++ ProjektMappen/MMMI/src/mmmi/UI/Main/FindCase/FindCaseController.java
@@ -8,7 +8,21 @@ package mmmi.UI.Main.FindCase;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import mmmi.Domain.Department;
+import mmmi.Domain.Interfaces.IDomain;
 
 /**
  * FXML Controller class
@@ -17,12 +31,42 @@ import java.util.ResourceBundle;
  */
 public class FindCaseController implements Initializable {
 
+    @FXML
+    private TextField TF_searchText;
+    @FXML
+    private ListView<?> LV_searchCaseList;
+    @FXML
+    private Button BTN_search;
+    @FXML
+    private RadioButton RB_searchOptionCitizen;
+    @FXML
+    private ToggleGroup searchOption;
+    @FXML
+    private RadioButton RB_searchOptionCaseNo;
+
+    IDomain domain; // Singleton?
+    ObservableList<Map<String, String>> obslistResult;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        // domain = Domain::getInstance(); Domain facade instantiation, how?
+        RB_searchOptionCaseNo.setUserData("Case");
+        RB_searchOptionCitizen.setUserData("Citizen");
+
+    }
+
+    @FXML
+    private void actionEventHandler(ActionEvent event) {
+        if(event.getSource() == BTN_search) {
+            String searchKey = searchOption.getSelectedToggle().getUserData().toString();
+            String searchValue = TF_searchText.getText();
+            List<Map<String, String>> result = domain.Search(searchKey, searchValue);
+            obslistResult = FXCollections.observableArrayList(result);
+        }
     }
 
 }

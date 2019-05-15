57c37870b46c73df8fb52646dea9b1e2eaf6f203
Unknown <mjako18@student.sdu.dk>
2019-05-15 23:18:34 +0200

    Search function
    Search function done in Domain

--- ProjektMappen/MMMI/src/mmmi/Domain/Department.java
+++ ProjektMappen/MMMI/src/mmmi/Domain/Department.java
@@ -2,13 +2,14 @@ package mmmi.Domain;
 
 import MMMI.Data_layer.DataHandler;
 import MMMI.Data_layer.Interfaces.IDataHandler;
+import MMMI.Data_layer.SearchCase;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import mmmi.Domain.Interfaces.IDomain;
 
-public class Department implements IDomain{
+public class Department implements IDomain {
 
     private final int id;
 
@@ -50,13 +51,30 @@ public class Department implements IDomain{
         throw new UnsupportedOperationException();
 
     }
+
     //REMEMBER: to use the correct names from the IDataHandler interface.
     //REMEMBER: check rights ask the people and dont just do some random stuff.
     @Override
     public List<Map<String, String>> Search(String key, String value) {
         //TODO: needs to be able to send all info from a searchcase to the GUI in the form of list with a map
         IDataHandler searchHandler = new DataHandler();
-        List searchCases = searchHandler.search(key, value);
+        List<SearchCase> searchCases = searchHandler.search(key, value);
+        int length = searchCases.size();
+        String caseID, citizenName, caseStatus, caseReason, currentCaseDate, caseEmployeeName, listString;
+        Map searchResultMap = new HashMap();
+        List searchResultList = new ArrayList();
+        for (int i = 0; i < length; i++) {
+            caseID = searchCases.get(i).getCaseID();
+            citizenName = searchCases.get(i).getCitizenName();
+            caseStatus = searchCases.get(i).getCaseStatus();
+            currentCaseDate = searchCases.get(i).getCurrentCaseDate();
+            caseReason = searchCases.get(i).getCaseReason();
+            caseEmployeeName = searchCases.get(i).getCaseEmployeeName();
+            listString = citizenName + " " + currentCaseDate + " " + caseReason + " " + caseEmployeeName + " " + caseStatus;
+            searchResultMap.put(caseID, listString);
+            searchResultList.add(searchResultMap);
+        }
+        return searchResultList;
     }
 
     @Override
@@ -76,7 +94,5 @@ public class Department implements IDomain{
         //TODO: Sends the data of employee with rights 
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
 
 }

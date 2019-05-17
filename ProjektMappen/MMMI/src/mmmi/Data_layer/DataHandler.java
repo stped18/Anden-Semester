fd01d1888317b8288d1c822c58ed3ded6b381392
Unknown <mjako18@student.sdu.dk>
2019-05-17 21:10:49 +0200

    1st cut
    Haven't tested it, but queries should work.

--- ProjektMappen/MMMI/src/mmmi/Data_layer/DataHandler.java
+++ ProjektMappen/MMMI/src/mmmi/Data_layer/DataHandler.java
@@ -2,7 +2,12 @@ package MMMI.Data_layer;
 
 import Data_layer.Connection.DatabaseConnection;
 import MMMI.Data_layer.Interfaces.IDataHandler;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DataHandler extends DatabaseConnection implements IDataHandler{
 
@@ -53,7 +58,66 @@ public class DataHandler extends DatabaseConnection implements IDataHandler{
     
     @Override
     public List<SearchCase> search(String searchKey, String searchValue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<SearchCase> sc = new ArrayList();
+        PreparedStatement searchCaseStmt = null;
+        String selectQuery, fromQuery, whereQuery, query;
+        if(searchKey == "case") {
+            try {
+                selectQuery = "SELECT c.caseid, c.status, ci.citizenid, CONCAT(ci.firstname, ' ', ci.lastname) AS citizenName, cc.datestamp, cc.regardingInquiry, CONCAT(e.firstname, ' ', e.lastname) AS employeeName, e.employeeid";
+                fromQuery = "FROM \"case\" as c, citizen AS ci, (SELECT casecaseid, datestamp, regardingInquiry FROM case_contents WHERE casecaseid = ? ORDER BY datestamp DESC LIMIT 1) AS cc, (SELECT casecaseid, employeeemployeeid FROM case_employee WHERE casecaseid = ?) AS ce, employee AS e";
+                whereQuery = "WHERE ce.casecaseid = c.caseid AND e.employeeid = ce.employeeemployeeid AND cc.casecaseid = c.caseid AND ci.citizenid = c.citizenregardingcitizenid AND c.caseid = ? AND c.departmentdepartmentid = ?;";
+                query = selectQuery + fromQuery + whereQuery;
+                
+                searchCaseStmt = dbConnection.prepareStatement(query);
+                searchCaseStmt.setString(1, searchValue);
+                searchCaseStmt.setString(2, searchValue);
+                searchCaseStmt.setString(3, searchValue);
+                searchCaseStmt.setString(4, ""); // departmentID from somewhere..
+                dbResultSet = searchCaseStmt.executeQuery();
+                while(dbResultSet.next()) {
+                    sc.add(new SearchCase(dbResultSet.getInt(3), dbResultSet.getString(4), dbResultSet.getString(1), dbResultSet.getString(2), dbResultSet.getString(5), dbResultSet.getString(6), dbResultSet.getInt(8), dbResultSet.getString(7)));
+                }
+            } catch (SQLException ex) {
+                System.out.println("readCitizen:\nSQLState: " + ex.getSQLState() + "\nmessage: " + ex.getMessage());
+                Logger.getLogger(DataHandler.class.getName()).log(Level.SEVERE, null, ex);
+                disConnectet();
+            } finally {
+                if(searchCaseStmt != null) {
+                    searchCaseStmt = null;
+                }
+                disConnectet();
+            }
+        } else if(searchKey == "citizen") {
+            try {
+                selectQuery = "SELECT c.caseid, c.status, ci.citizenid, CONCAT(ci.firstname, ' ', ci.lastname) AS citizenName, cc.datestamp, cc.regardingInquiry, "
+                            + "CONCAT(e.firstname, ' ', e.lastname) AS employeeName, e.employeeid";
+                fromQuery = "FROM citizen AS ci, \"case\" AS c, zipcode AS z, employee AS e, "
+                          + "(SELECT casecaseid, datestamp "
+                            + "FROM case_contents, \"case\" AS c "
+                            + "WHERE casecaseid = c.caseid AND c.citizenregardingcitizenid = 1 ORDER BY datestamp DESC LIMIT 1) AS cc, "
+                          + "(SELECT casecaseid, employeeemployeeid "
+                            + "FROM case_employee, \"case\" "
+                            + "WHERE casecaseid = caseid AND citizenregardingcitizenid = 1) AS ce";
+                whereQuery = "WHERE z.zipcode = ci.zipcodezipcode AND cc.casecaseid = c.caseid AND ce.casecaseid = c.caseid AND ce.employeeemployeeid = e.employeeid "
+                           + "AND (CONCAT(ci.firstname, ' ', ci.lastname) LIKE ? OR (CONCAT(ci.streetname, ' ', ci.houseno) LIKE ? AND CONCAT(ci.zipcodezipcode, ' ', z.cityname) LIKE ?)) "
+                           + "AND ci.citizenid = c.citizenregardingcitizenid AND c.departmentdepartmentid = ?";
+                query = selectQuery + fromQuery + whereQuery;
+                dbResultSet = searchCaseStmt.executeQuery(query);
+                while(dbResultSet.next()) {
+                    sc.add(new SearchCase(dbResultSet.getInt(3), dbResultSet.getString(4), dbResultSet.getString(1), dbResultSet.getString(2), dbResultSet.getString(5), dbResultSet.getString(6), dbResultSet.getInt(8), dbResultSet.getString(7)));
+                }
+            } catch (SQLException ex) {
+                System.out.println("search:\nSQLState: " + ex.getSQLState() + "\nmessage: " + ex.getMessage());
+                Logger.getLogger(DataHandler.class.getName()).log(Level.SEVERE, null, ex);
+                disConnectet();
+            } finally {
+                if(searchCaseStmt != null) {
+                    searchCaseStmt = null;
+                }
+                disConnectet();
+            }
+        }
+        return sc;
     }
 
 }
--- ProjektMappen/MMMI/src/mmmi/Data_layer/SearchCase.java
+++ ProjektMappen/MMMI/src/mmmi/Data_layer/SearchCase.java
@@ -4,70 +4,67 @@ import java.util.Date;
 
 public class SearchCase {
 
-    private String citizenName;
-    private int citizenID;
-    private String caseID;
-    private String caseStatus;
-    private Date date;
-    private String reason;
-    private String EmployeeName;
-    private int EmployeeID;
+    private final int citizenID;
+    private final String citizenName;
+    private final String caseID;
+    private final String caseStatus;
+    private final String date;
+    private final String reason;
+    private final int EmployeeID;
+    private final String EmployeeName;
 
     /**
      *
+     * @param citizenID
+     * @param citizenName
+     * @param caseID
+     * @param caseStatus
+     * @param date
+     * @param reason
+     * @param EmployeeID
+     * @param EmployeeName
      */
-    public SearchCase() {
-        // TODO - implement SearchCase.SearchCase
-        throw new UnsupportedOperationException();
+    public SearchCase(int citizenID, String citizenName, String caseID, String caseStatus, String date, String reason, int EmployeeID, String EmployeeName) {
+        this.citizenID = citizenID;
+        this.citizenName = citizenName;
+        this.caseID = caseID;
+        this.caseStatus = caseStatus;
+        this.date = date;
+        this.reason = reason;
+        this.EmployeeID = EmployeeID;
+        this.EmployeeName = EmployeeName;
     }
 
-    /**
-     * @return
-     */
-    public Citizen getCitizen() {
-        throw new UnsupportedOperationException();
+    public int getCitizenID() {
+        return citizenID;
+    }
+    
+    public String getCitizenName() {
+        return citizenName;
     }
 
-    /**
-     * @return
-     */
-    public int getCaseNumber() {
-        throw new UnsupportedOperationException();
+    public String getCaseID() {
+        return caseID;
     }
 
-    /**
-     * @return
-     */
     public String getCaseStatus() {
-        return this.caseStatus;
+        return caseStatus;
     }
 
-    /**
-     * @return
-     */
-    public Date getDate() {
-        return this.date;
+    public String getDate() {
+        return date;
     }
 
-    /**
-     * @return
-     */
     public String getReason() {
-        return this.reason;
+        return reason;
     }
-
-    /**
-     * @return
-     */
-    public String getEmployeeName() {
-        return this.EmployeeName;
-    }
-
-    /**
-     * @return
-     */
+    
     public int getEmployeeID() {
-        return this.EmployeeID;
+        return EmployeeID;
+    }
+    
+    public String getEmployeeName() {
+        return EmployeeName;
     }
 
 }

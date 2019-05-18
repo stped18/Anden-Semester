2af8ccabb18b3aeffd53bd5477faa7bba5f00cfa
Steffen Vitten <Steffen@vittech.dk>
2019-05-19 00:28:15 +0200

    prossbar

--- ProjektMappen/MMMI/src/LoginSystem/ViewLayer/LoginSystemController.java
+++ ProjektMappen/MMMI/src/LoginSystem/ViewLayer/LoginSystemController.java
@@ -18,6 +18,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -49,6 +50,8 @@ public class LoginSystemController implements Initializable {
     @FXML
     private AnchorPane fxErroBoks;
     private MMMI main;
+    @FXML
+    private ProgressBar fxprocessbar;
 
     /**
      * Initializes the controller class.
@@ -56,6 +59,14 @@ public class LoginSystemController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         fxErroBoks.setVisible(false);
+           try {   
+                process();
+                 double d = fxprocessbar.getProgress();
+                 fxprocessbar.setProgress(d);
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(LoginSystemController.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
 
     @FXML
@@ -64,16 +75,21 @@ public class LoginSystemController implements Initializable {
             loginSystem = new LoginSystem();
             loginSystem.setUsername(fxUsername.getText());
             loginSystem.setPassword(fxPassword.getText());
+                 
+         
             if (loginSystem.getEmployee()) {
                 loginSystem.sendEmployee();
                 MMMI main = new MMMI();
+
                 try {
                     main.changeScene();
+
                     Stage stage = (Stage) fxloginroot.getScene().getWindow();
+
                     stage.close();
                 } catch (IOException ex) {
                     Logger.getLogger(LoginSystemController.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                } 
 
             } else {
                 fxErroBoks.setVisible(true);
@@ -85,6 +101,60 @@ public class LoginSystemController implements Initializable {
 
     }
 
+//    private void proces() {
+//        new Thread(() -> {
+//            for (double i = 0.01; i < 1.00; i++) {
+//                fxprocessbar.setProgress(i);
+//                try {
+//                    Thread.sleep(10);
+//                } catch (InterruptedException ex) {
+//                    Logger.getLogger(LoginSystemController.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//                if (i == 1.00) {
+//                    i = 0.00;
+//                }
+////        try {
+////            Thread.sleep(300);
+////        } catch (InterruptedException e) {
+////            e.printStackTrace();
+////        }
+////        final double progress = i * 0.1;
+////        
+////        Platform.runLater(() -> ui.updateProgress(progress));
+//
+//            }
+//        }).start();
+//    }
+    private void process() {
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                final double progress = i * 0.1;
+                
+                Platform.runLater(() -> updateProgress(progress));
+
+            }
+        }).start();
+
+    }
+
+    public void updateProgress(double progress) {
+
+        fxprocessbar.setProgress(progress);
+        double d = fxprocessbar.getProgress();
+        System.out.println("progress APPARENTLY set to: " + d);
+
+    }
 }
+
+
+
+
+
 
 

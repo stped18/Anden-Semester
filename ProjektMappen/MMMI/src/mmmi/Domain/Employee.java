4a6f21b02e4ed65073c09698aa4b7fe10cbd5f31
Aleksander Henriksen <a_henriksen@outlook.com>
2019-05-18 17:31:46 +0200

    updated Employee object in domain with methods

--- ProjektMappen/MMMI/src/mmmi/Domain/Employee.java
+++ ProjektMappen/MMMI/src/mmmi/Domain/Employee.java
@@ -4,52 +4,47 @@ import java.util.Map;
 
 public class Employee {
 
-    private int id;
-    private String firstName;
-    private String lastname;
-    private int departmentID;
-    private int roleID;
-    private Map<Integer, String> rightsMap;
+    private final int employeeID;
+    private final String name;
+    private final int roleID;
+    private final int departmentID;
+    private final Map<String, String> employeeCases;
+    private final Map<Integer, String> rights;
 
-    public Employee() {
-    }
-
-    public Employee(int id, String firstName, String lastname, int departmentID, int roleID, Map<Integer, String> rightsMap) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastname = lastname;
-        this.departmentID = departmentID;
+    public Employee(int employeeID, String name, int roleID, int departmentID, Map<String, String> employeeCases, Map<Integer, String> rights) {
+        this.employeeID = employeeID;
+        this.name = name;
         this.roleID = roleID;
-        this.rightsMap = rightsMap;
+        this.departmentID = departmentID;
+        this.employeeCases = employeeCases;
+        this.rights = rights;
     }
 
-    public int getId() {
-        return this.id;
+    public int getEmployeeID() {
+        return this.employeeID;
     }
 
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastname() {
-        return this.lastname;
-    }
-
-    public int getDepartmentID() {
-        return this.departmentID;
+    public String getName() {
+        return this.name;
     }
 
     public int getRoleID() {
         return this.roleID;
     }
 
-    public Map<Integer, String> getRightsMap() {
-        return this.rightsMap;
+    public int getDepartmentID() {
+        return this.departmentID;
+    }
+
+    public Map<String, String> getEmployeeCases() {
+        return this.employeeCases;
+    }
+
+    public Map<Integer, String> getrights() {
+        return this.rights;
     }
 
     public boolean checkRight(int rightID) {
-        return this.rightsMap.containsKey(rightID);
+        return this.rights.containsKey(rightID);
     }
-    
-    // TODO: Need something to store employee cases in.
 }

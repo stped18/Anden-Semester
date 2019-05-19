0a760468c3aa9ba2cf12d9135e5e696d5ace9710
Unknown <mjako18@student.sdu.dk>
2019-05-19 12:59:56 +0200

    Changed listview
    Changed the listview to a tableview.
    Created a SearchResult class to hold the data from domain layer.
    Updated controller to reflect change to tableview

--- ProjektMappen/MMMI/src/mmmi/UI/Main/FindCase/FindCaseController.java
+++ ProjektMappen/MMMI/src/mmmi/UI/Main/FindCase/FindCaseController.java
@@ -3,16 +3,23 @@ package mmmi.UI.Main.FindCase;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -25,8 +32,6 @@ import mmmi.Domain.Interfaces.IDomain;
  */
 public class FindCaseController implements Initializable {
 
-    @FXML
-    private ListView<String> LV_searchCaseList;
     @FXML
     private Button BTN_search;
     @FXML
@@ -53,8 +58,22 @@ public class FindCaseController implements Initializable {
     private TextField TF_searchCitizenCPR;
     
     IDomain domain; // Singleton?
-    ObservableList<String> obslistResult;
+    ObservableList<SearchResult> obslistResult;
     Map<String, Map<String, String>> result;
+    @FXML
+    private TableView<SearchResult> TV_searchCase;
+    @FXML
+    private TableColumn<SearchResult, String> caseIDColumn;
+    @FXML
+    private TableColumn<SearchResult, String> citizenNameColumn;
+    @FXML
+    private TableColumn<SearchResult, String> caseStatusColumn;
+    @FXML
+    private TableColumn<SearchResult, String> currentDateColumn;
+    @FXML
+    private TableColumn<SearchResult, String> employeeNameColumn;
+    @FXML
+    private TableColumn<SearchResult, String> reasonColumn;
     
 
     /**
@@ -71,7 +90,16 @@ public class FindCaseController implements Initializable {
         RB_searchOptionCaseNo.setUserData("Case");
         RB_searchOptionCitizen.setUserData("Citizen");
         obslistResult = FXCollections.observableArrayList();
-        LV_searchCaseList.setItems(obslistResult);
+        TV_searchCase.setPlaceholder(new Label());
+        TV_searchCase.setItems(obslistResult);
+        caseIDColumn.setCellValueFactory(cellData -> cellData.getValue().caseIDProperty());
+        citizenNameColumn.setCellValueFactory(cellData -> cellData.getValue().citizenNameProperty());
+        caseStatusColumn.setCellValueFactory(cellData -> cellData.getValue().caseStatusProperty());
+        currentDateColumn.setCellValueFactory(cellData -> cellData.getValue().currentCaseDateProperty());
+        employeeNameColumn.setCellValueFactory(cellData -> cellData.getValue().employeeNameProperty());
+        reasonColumn.setCellValueFactory(cellData -> cellData.getValue().reasonProperty());
+        /*TV_searchCase.getSelectionModel().selectedItemProperty().addListener(
+            (observable, oldValue, newValue) -> openCase(newValue));*/
     }
 
     /**
@@ -96,18 +124,17 @@ public class FindCaseController implements Initializable {
             if(!searchValue.equals("") && !searchValue.equals("%%%")) {
                 result = domain.search(searchKey, searchValue);
                 for (Map.Entry<String, Map<String, String>> map : result.entrySet()) {
-                    String info = "";
-                    for (Map.Entry<String, String> entry : map.getValue().entrySet()) {
-                        info += entry.getValue() + " ";
-                    }
-                    obslistResult.add(info);
+                    System.out.println(map);
+                    String caseid = map.getKey();
+
+                    obslistResult.add(new SearchResult(map.getValue().get("citizenName"), caseid, map.getValue().get("caseStatus"), map.getValue().get("currentCaseDate"), map.getValue().get("caseReason"), map.getValue().get("caseEmployeeName")));
                 }
-                LV_searchCaseList.refresh();
+                TV_searchCase.refresh();
             } else {
                 //Error, no data input
             }
         } else if (event.getSource() == BTN_openCase) {
-            openCase();
+            openCase(TV_searchCase.getSelectionModel().getSelectedItem());
         } else if (event.getSource() == RB_searchOptionCaseNo) {
             caseNumberPane.setVisible(true);
             citizenPane.setVisible(false);
@@ -117,27 +144,18 @@ public class FindCaseController implements Initializable {
         }
     }
 
-    private void openCase() {
-        int listviewIndex = LV_searchCaseList.getFocusModel().getFocusedIndex();
-        if(listviewIndex > -1) {
-            Map<String, String> m = result.get(listviewIndex);
-            String caseID = m.keySet().toString();
-            System.out.println(caseID);
+    private void openCase(SearchResult sr) {
+        System.out.println(sr.getCaseID());
+//            for(int i=0;i<=listviewIndex;i++) {
+//                if(i == listviewIndex) {
+//                    Set k = result.keySet();
+//                    
+//                }
+//            }
+            //Map<String, String> m = result.get("caseid");
+            //String caseID = m.keySet().toString();
+            //System.out.println(caseID);
             // Open case
-        }
-    }
-
-    /**
-     *
-     * @param event
-     */
-    private void mouseEventHandler(MouseEvent event) {
-
-        if (event.getSource() == LV_searchCaseList) {
-            if (event.getClickCount() == 2) {
-                openCase();
-            }
-        }
     }
 
 }

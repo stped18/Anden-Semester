9227ea2078ed1d3f8941f60c309febce5e6e0983
Per Terp <pertl18@gmail.com>
2019-05-21 12:17:35 +0200

    write employee gone

--- ProjektMappen/MMMI/src/mmmi/Data_layer/Interfaces/IDataHandler.java
+++ ProjektMappen/MMMI/src/mmmi/Data_layer/Interfaces/IDataHandler.java
@@ -16,7 +16,7 @@ public interface IDataHandler {
 
     public boolean writeCase(Case theCase);
     public int writeCitizen(Citizen citizen);
-    public boolean writeEmployee(Employee employee);
+    //public boolean writeEmployee(Employee employee);
     public boolean writeAlternativeNote(int caseID, String note);
 
     public boolean updateCitizen(Citizen citizen);
